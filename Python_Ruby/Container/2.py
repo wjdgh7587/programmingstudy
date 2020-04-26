b38dc34f206124179ed632488086d739d6526214
Jeongho Yun <wjdgh7587@gmail.com>
2020-04-27 03:47:45 +0900

    recording_container_2

---
+++ Python_Ruby/Container/2.py
@@ -0,0 +1,6 @@
+al = ['A' , 'B', 'C', 'D']
+print(len(al)) #4
+al.append('E')
+print(al)
+del(al[0])
+print(al)
---
+++ Python_Ruby/Container/2.rb
@@ -0,0 +1,9 @@
+#!/usr/bin/ruby
+
+al = ['A', 'B', 'C', 'D']
+puts(al.length) # 4
+al.push('E')
+print(al) # ["A", "B", "C", "D", "E"]
+al.delete_at(0)
+print("\n")
+print(al) # ["B", "C", "D", "E"]
